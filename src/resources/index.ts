aa6353bdda98231fb5fa01895b3821c42e5a41ee
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-03-26 10:44:00 +1000

    feat(skeleton): loader

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,5 +1,8 @@
+import { PLATFORM } from 'aurelia-pal';
 import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
-  //config.globalResources([]);
+  config.globalResources([
+      PLATFORM.moduleName('./elements/loader.html')
+  ]);
 }
